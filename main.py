a789d2ae7c9112609902e70919699f7e524e417b
drdon1234 <96719161+drdon1234@users.noreply.github.com>
2025-04-14 23:01:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
-from astrbot.api.message_components import Plain, Video, Node, Nodes
+from astrbot.api.message_components import Nodes
 from astrbot.core.star.filter.event_message_type import EventMessageType
 from .parser import DouyinParser
 
 @register("astrbot_plugin_douyin_bot", "drdon1234", "自动识别抖音链接并转换为视频直链", "1.0")
 class DouyinBotPlugin(Star):
-    def __init__(self, context: Context):
+    def __init__(self, context: Context, config: dict):
         super().__init__(context)
+        self.is_auto_parse = config.get("is_auto_parse", True)
         self.parser = DouyinParser()
 
     async def terminate(self):
         pass
-    
-    @filter.event_message_type(EventMessageType.ALL)
-    async def auto_parse(self, event: AstrMessageEvent):
+
+    async def parse(self, event: AstrMessageEvent):
         nodes = await self.parser.build_nodes(event)
         if nodes is None:
             return
-        yield event.plain_result("抖音bot为您服务 ٩( 'ω' )و")
+        await event.send(event.plain_result("抖音bot为您服务 ٩( 'ω' )و"))
         await event.send(event.chain_result([Nodes(nodes)]))
+    
+    @filter.event_message_type(EventMessageType.ALL)
+    async def auto_parse(self, event: AstrMessageEvent):
+        if self.is_auto_parse:
+            await self.parse(event)
 
     @filter.command("抖音解析")
     async def specified_parse(self, event: AstrMessageEvent):
-        pass
+        await self.parse(event)
+
